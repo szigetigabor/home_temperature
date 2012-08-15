94e9e4c94bc096b64a821c16d0c49413fffedaf8
Gabor Szigeti <szigeti@niif.hu>
2012-08-15 12:21:04 +0200

    Add title for the graphs.

--- logging/update_temp.sh
+++ logging/update_temp.sh
@@ -16,11 +16,11 @@ do
   rrdtool update temperature5004_$line.rrd N:$temp
 
   # Create graphs
-  rrdtool graph /var/www/temp_graphs/$line/temp_h.png --start -1h DEF:temp=temperature5004_$line.rrd:temp:AVERAGE LINE1:temp#0000FF:"Last temperature [deg C]" GPRINT:temp:LAST:"%2.2lf%sC"
-  rrdtool graph /var/www/temp_graphs/$line/temp_d.png --start -1d DEF:temp=temperature5004_$line.rrd:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]"
-  rrdtool graph /var/www/temp_graphs/$line/temp_w.png --start -1w DEF:temp=temperature5004_$line.rrd:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]"
-  rrdtool graph /var/www/temp_graphs/$line/temp_m.png --start -1m DEF:temp=temperature5004_$line.rrd:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]"
-  rrdtool graph /var/www/temp_graphs/$line/temp_y.png --start -1y DEF:temp=temperature5004_$line.rrd:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]"
+  rrdtool graph /var/www/temp_graphs/$line/temp_h.png --start -1h --title "Hourly graph" DEF:temp=temperature5004_$line.rrd:temp:AVERAGE LINE1:temp#0000FF:"Last temperature [deg C]" GPRINT:temp:LAST:"%2.2lf%sC"
+  rrdtool graph /var/www/temp_graphs/$line/temp_d.png --start -1d --title "Daily graph" DEF:temp=temperature5004_$line.rrd:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]"
+  rrdtool graph /var/www/temp_graphs/$line/temp_w.png --start -1w --title "Weekly graph" DEF:temp=temperature5004_$line.rrd:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]"
+  rrdtool graph /var/www/temp_graphs/$line/temp_m.png --start -1m --title "Monthly graph" DEF:temp=temperature5004_$line.rrd:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]"
+  rrdtool graph /var/www/temp_graphs/$line/temp_y.png --start -1y --title "Yearly graph" DEF:temp=temperature5004_$line.rrd:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]"
   #0000FF means blue trace color in the graphs.
 
 done
