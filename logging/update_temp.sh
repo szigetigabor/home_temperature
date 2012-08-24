c951569518481dba9eb0919404ee37c6678182e8
Gabor Szigeti <szigeti@niif.hu>
2012-08-24 09:36:18 +0200

    Set directory access and the current value to a local file.

--- logging/update_temp.sh
+++ logging/update_temp.sh
@@ -6,9 +6,17 @@ sensors=`cat /sys/bus/w1/devices/w1_bus_master1/w1_master_slaves;`
 
 for line in $sensors
 do
+   if [ ! -d $line ]; then
+      mkdir $line
+      chmod 777 $line
+   fi
   tempread=`cat /sys/bus/w1/devices/w1_bus_master1/$line/w1_slave|tail -1|cut -d"=" -f2;`  
+  # update store values
+  echo $tempread > $line/value;
+
   l=`expr length $tempread`
-  up=`echo $tempread|cut -c1-$(($l-3));`
+  #up=`echo $tempread|cut -c1-$(($l-3));`
+  up=`echo $(($tempread/1000));`
   down=`echo $tempread|cut -c $(($l-2))-;`
   temp=$up.$down
 
@@ -22,6 +30,6 @@ do
   rrdtool graph /var/www/temp_graphs/$line/temp_m.png --start -1m --title "Monthly graph" DEF:temp=temperature5004_$line.rrd:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]" GPRINT:temp:MIN:"min %2.2lf%sC" GPRINT:temp:LAST:"last %2.2lf%sC" GPRINT:temp:MAX:"max %2.2lf%sC"
   rrdtool graph /var/www/temp_graphs/$line/temp_y.png --start -1y --title "Yearly graph" DEF:temp=temperature5004_$line.rrd:temp:AVERAGE LINE1:temp#0000FF:"Temperature [deg C]" GPRINT:temp:MIN:"min %2.2lf%sC" GPRINT:temp:LAST:"last %2.2lf%sC" GPRINT:temp:MAX:"max %2.2lf%sC"
   #0000FF means blue trace color in the graphs.
-
+  sleep 1
 done
 
