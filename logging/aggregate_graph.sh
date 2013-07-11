76f49cb0e3f61c6ce646597ce50ff8434825ed57
Szigeti Gabor <szigeti@niif.hu>
2013-07-11 13:36:56 +0200

    Show the owfs sensors on tha page. part 3.

--- logging/aggregate_graph.sh
+++ logging/aggregate_graph.sh
@@ -25,25 +25,29 @@ db_prefix="temperature5004_"
 cd $sensor_settings_path
 
 # Read temperature from sensors
-sensors=`cat /sys/bus/w1/devices/w1_bus_master1/w1_master_slaves;`
+sensors=`cat /sys/bus/w1/devices/w1_bus_master1/w1_master_slaves; ls /tmp/1wire`
 
 parameters="graph /var/www/temp_graphs/aggr_temp_${1}.png --start -1${1} --title ${title[$1]}_graph"
 #graph colors
-colors=( "#0000FF" "#CCCCCC" "#00FF00"  "#FF0000" "#8800FF" "#00FFFF" "#888800" "#008888")
+colors=( "#0000FF" "#CCCCCC" "#00FF00"  "#FF0000" "#8800FF" "#00FFFF" "#888800" "#008888" "#FF00FF" "#123456" )
 i=0
 for line in $sensors
 do
-    if [ `echo $line|cut -c1-3` != "28-" ]; then
+    if [ `echo $line|cut -c1-2` != "28" ]; then
        continue
     fi
 
     alias=$line
     if [ -e $line/alias ]; then
       alias=`cat $line/alias`
+    else
+      if [ `echo $line|cut -c1-3` = "28." ]; then
+        alias=`echo  $line | tr '.' '-'`
+      fi
     fi
     # Create rrdtool parameters
-    parameters="${parameters} DEF:temp${line}=${db_prefix}${line}.rrd:temp:AVERAGE LINE1:temp${line}${colors[$i]}:$alias \
-              GPRINT:temp${line}:MIN:min\:%2.2lf%sC GPRINT:temp${line}:LAST:last\:%2.2lf%sC GPRINT:temp${line}:MAX:max\:%2.2lf%sC\n "
+    parameters="${parameters} DEF:temp${alias}=${db_prefix}${line}.rrd:temp:AVERAGE LINE1:temp${alias}${colors[$i]}:$alias \
+              GPRINT:temp${alias}:MIN:min\:%2.2lf%sC GPRINT:temp${alias}:LAST:last\:%2.2lf%sC GPRINT:temp${alias}:MAX:max\:%2.2lf%sC\n "
     let "i=i+1"
 done
 
