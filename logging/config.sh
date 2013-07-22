b05df1af6be19c3df95e1783a1cbe2ffe29ec455
Szigeti Gabor <szigeti@niif.hu>
2013-07-22 14:25:29 +0200

    Forget config script

--- logging/config.sh
+++ logging/config.sh
@@ -10,3 +10,18 @@ colors=( "#0000FF" "#CCCCCC" "#00FF00"  "#FF0000" "#8800FF" "#00FFFF" "#888800" 
 sensors=`cat $sensors_path;`
 sensors_owfs=`ls $sensors_owfs_path;`
 sensors_all=`cat $sensors_path; ls $sensors_owfs_path;`
+
+typeset -A title
+title['h']="Hourly_graph"
+title['d']="Daily_graph"
+title['w']="Weekly_graph"
+title['m']="Monthly_graph"
+title['y']="Yearly_graph"
+
+typeset -A delay
+delay['h']=300
+delay['d']=3600
+delay['w']=86400    #delay['d'] * 24
+delay['m']=604800   #delay['w'] * 7
+delay['y']=2419200  #delay['d'] * 4
+
