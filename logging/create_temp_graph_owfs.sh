21bad90213afe6e7787959ce210fcb133f2711a2
Szigeti Gabor <szigeti@niif.hu>
2013-07-22 14:24:02 +0200

    Do not generate graphs so often.

--- logging/create_temp_graph_owfs.sh
+++ logging/create_temp_graph_owfs.sh
@@ -29,45 +29,40 @@ do
   now=`date +%s`
   file=$www_path/$line/temp_h.png
   mode_time=`stat -c %Y $file`
-  delay=5
-  if [ $(echo "$now - $mode_time" | bc) -gt $delay ]; then
-    rrdtool graph $file --start -1h --title "Hourly graph" \
+  if [ $(echo "$now - $mode_time" | bc) -gt ${delay['h']} ]; then
+    rrdtool graph $file --start -1h --title ${title['h']} \
        --watermark "`date`" \
        --vertical-label "Temperature (C)" \
        DEF:temp=$DB:temp:AVERAGE LINE1:temp#0000FF:"$alias" GPRINT:temp:MIN:"min %2.2lf%sC" GPRINT:temp:LAST:"last %2.2lf%sC" GPRINT:temp:MAX:"max %2.2lf%sC"
   fi
   file=$www_path/$line/temp_d.png
   mode_time=`stat -c %Y $file`
-  delay=60
-  if [ $(echo "$now - $mode_time" | bc) -gt $delay ]; then
-    rrdtool graph $file --start -1d --title "Daily graph" \
+  if [ $(echo "$now - $mode_time" | bc) -gt ${delay['d']} ]; then
+    rrdtool graph $file --start -1d --title ${title['d']} \
        --watermark "`date`" \
        --vertical-label "Temperature (C)" \
        DEF:temp=$DB:temp:AVERAGE LINE1:temp#0000FF:"$alias" GPRINT:temp:MIN:"min %2.2lf%sC" GPRINT:temp:LAST:"last %2.2lf%sC" GPRINT:temp:MAX:"max %2.2lf%sC"
   fi
   file=$www_path/$line/temp_w.png
   mode_time=`stat -c %Y $file`
-  delay=$(echo "$delay * 24" | bc)
-  if [ $(echo "$now - $mode_time" | bc) -gt $delay ]; then
-    rrdtool graph $file --start -1w --title "Weekly graph" \
+  if [ $(echo "$now - $mode_time" | bc) -gt ${delay['w']} ]; then
+    rrdtool graph $file --start -1w --title ${title['w']} \
        --watermark "`date`" \
        --vertical-label "Temperature (C)" \
        DEF:temp=$DB:temp:AVERAGE LINE1:temp#0000FF:"$alias" GPRINT:temp:MIN:"min %2.2lf%sC" GPRINT:temp:LAST:"last %2.2lf%sC" GPRINT:temp:MAX:"max %2.2lf%sC"
   fi
   file=$www_path/$line/temp_m.png
   mode_time=`stat -c %Y $file`
-  delay=$(echo "$delay * 7" | bc)
-  if [ $(echo "$now - $mode_time" | bc) -gt $delay ]; then
-    rrdtool graph $file --start -1m --title "Monthly graph" \
+  if [ $(echo "$now - $mode_time" | bc) -gt ${delay['m']} ]; then
+    rrdtool graph $file --start -1m --title ${title['m']} \
        --watermark "`date`" \
        --vertical-label "Temperature (C)" \
        DEF:temp=$DB:temp:AVERAGE LINE1:temp#0000FF:"$alias" GPRINT:temp:MIN:"min %2.2lf%sC" GPRINT:temp:LAST:"last %2.2lf%sC" GPRINT:temp:MAX:"max %2.2lf%sC"
   fi
   file=$www_path/$line/temp_y.png
   mode_time=`stat -c %Y $file`
-  delay=$(echo "$delay * 4" | bc)
-  if [ $(echo "$now - $mode_time" | bc) -gt $delay ]; then
-    rrdtool graph $file --start -1y --title "Yearly graph" \
+  if [ $(echo "$now - $mode_time" | bc) -gt ${delay['y']} ]; then
+    rrdtool graph $file --start -1y --title ${title['y']} \
        --watermark "`date`" \
        --vertical-label "Temperature (C)" \
        DEF:temp=$DB:temp:AVERAGE LINE1:temp#0000FF:"$alias" GPRINT:temp:MIN:"min %2.2lf%sC" GPRINT:temp:LAST:"last %2.2lf%sC" GPRINT:temp:MAX:"max %2.2lf%sC"
--- logging/create_volt_graph.sh
+++ logging/create_volt_graph.sh
@@ -7,12 +7,6 @@ containsElement () {
 }
 
 option=( "h" "d" "w" "m" "y" )
-typeset -A title
-title['h']="Hourly"
-title['d']="Daily"
-title['w']="Weekly"
-title['m']="Monthly"
-title['y']="Yearly"
 containsElement $1 "${option[@]}"
 if [ `echo $?` == "1" ]; then
   echo "Wrong parameter! [h, w, d, m, y]"
@@ -24,15 +18,18 @@ source $prefix/config_volt.sh
 
 cd $sensor_settings_path
 
-# Read lux from sensors
-
-parameters="graph $www_path/volt_${1}.png --start -1${1} --title ${title[$1]}_graph"
+# Read Volt from sensors
+file=$www_path/volt_${1}.png
+parameters="graph $file --start -1${1} --title ${title[$1]}"
 i=0
 line=""
 # Create rrdtool parameters
-parameters="${parameters} DEF:lux${line}=${db_prefix}${line}.rrd:lux:AVERAGE LINE1:lux${line}${colors[$i]}:$alias \
+now=`date +%s`
+mode_time=`stat -c %Y $file`
+if [ $(echo "$now - $mode_time" | bc) -gt ${delay[$1]} ]; then
+  parameters="${parameters} DEF:lux${line}=${db_prefix}${line}.rrd:lux:AVERAGE LINE1:lux${line}${colors[$i]}:$alias \
               GPRINT:lux${line}:MIN:min\:%5.2lf%sV GPRINT:lux${line}:LAST:last\:%2.2lf%sV GPRINT:lux${line}:MAX:max\:%2.2lf%sV\n "
 
-# Create graphs
-rrdtool $parameters --watermark "`date`" --vertical-label "Volt" --width 650 --height 300 --font DEFAULT:10: #--slope-mode
-
+  # Create graphs
+  rrdtool $parameters --watermark "`date`" --vertical-label "Volt" --width 650 --height 300 --font DEFAULT:10: #--slope-mode
+fi
