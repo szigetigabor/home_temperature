f5d4897681cff9194e729beb8a7fd4fdcade6fa3
Szigeti Gabor <szigeti@niif.hu>
2013-10-07 10:50:06 +0200

    Change the path of the owfs devices.

--- logging/config.sh
+++ logging/config.sh
@@ -2,7 +2,7 @@
 
 sensor_settings_path="/home/pi/logging"
 sensors_path="/sys/bus/w1/devices/w1_bus_master1/w1_master_slaves"
-sensors_owfs_path="/tmp/1wire"
+sensors_owfs_path="/mnt/1wire"
 
 www_path="/var/www/temp_graphs"
 colors=( "#0000FF" "#CCCCCC" "#00FF00"  "#FF0000" "#8800FF" "#00FFFF" "#888800" "#008888" "#FF00FF" "#123456" )
