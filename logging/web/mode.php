4394a2048998a21148fcc8b45d0ac4033e01b339
Szigeti Gabor <szigeti@niif.hu>
2014-03-20 08:46:51 +0100

    Separate the main modes from the anothers.

--- logging/web/mode.php
+++ logging/web/mode.php
@@ -18,12 +18,34 @@ $current_mode = trim($current_mode, " \n.");
 echo "<table id=\"mode\">";
 echo "<tr id=\"mode\">";
 
+global $off_modes_select;
+//print main modes
+foreach($off_modes_select as $mode)
+{
+  echo "<td id=\"mode\">";
+  echo "       <form method=\"post\">";
+  echo "         <input type=\"hidden\" name=\"mode\" value=\"$mode\" >";
+  $disable = "";
+  $class="buttonclass";
+  if ( $current_mode == $mode ) {
+    $disable = "disabled=true";
+    $class = "active$class";
+  }
+  echo "         <input type=\"submit\" value=\"$mode\" class=\"$class\" $disable >";
+  echo "       </form>";
+  echo "</td>";
+}
+echo "</tr><tr id=\"mode\">";
+
 //print each mode settings
 $i=0;
 foreach($modes as $mode)
 {
   $mode_name = substr($mode, strrpos($mode, "/")+1);
-  $mode_description = read_file($mode);
+  if (in_array($mode_name, $off_modes_select)) {
+    continue;
+  }
+  //$mode_description = read_file($mode);
   echo "<td id=\"mode\">";
   echo "       <form method=\"post\">";
   echo "         <input type=\"hidden\" name=\"mode\" value=\"$mode_name\" >";
