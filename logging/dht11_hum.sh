d8317a6335a922c37cbcae1c888f68e50cf818cb
Szigeti Gabor <szigeti@niif.hu>
2014-04-11 11:21:22 +0200

    dht22 support

--- logging/dht11_hum.sh
+++ logging/dht11_hum.sh
@@ -6,14 +6,22 @@
 type=$1
 port=$2
 
-while true; do
-  output=`./Adafruit_DHT $1 $2`
+prefix=$(dirname $0)
+source $prefix/config.sh
+
+for i in {1..10}; do
+  output=`$sensor_settings_path/Adafruit_DHT $1 $2`
 
   i=$((${#output}-1))
 
   if [ "${output:$i:1}" == "%" ]; then
     i=$((${#output}-4))
-    echo ${output:$i:2}
+    len=2
+    if [ $type == 22 ]; then
+      i=$((${#output}-6))
+      len=4
+    fi
+    echo ${output:$i:len}
 
     exit
   fi
